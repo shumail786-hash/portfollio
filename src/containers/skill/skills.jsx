fc4c7a914f1fa17059d3d931dd7f6a7430938604
unknown <sherrymines@outlook.com>
2022-03-11 16:40:44 +0500

    skills-ended

--- src/containers/skill/skills.jsx
+++ src/containers/skill/skills.jsx
@@ -21,7 +21,11 @@ const Skills = () => {
       setSkills(data);
     });
   }, []);
-  console.log(skills);
+
+  const experienceSort = experiences.sort((a, b) => {
+    return a.year - b.year;
+  });
+  // console.log(vari, "qurban");
   return (
     <div>
       <h2 className="head-text">Skills & Experience </h2>
@@ -43,6 +47,38 @@ const Skills = () => {
                 <img src={urlFor(skill.icon)} alt={skill.name} />
               </div>
               <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+        <motion.div className="app__skills-exp">
+          {experienceSort.map((experience) => (
+            <motion.div className="app__skills-exp-item" key={experience.year}>
+              <div className="app__skills-exp-year">
+                <p className="p-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <motion.div
+                    whileInView={{ opacity: [0, 1] }}
+                    transition={{ duration: 0.5 }}
+                    className="app__skills-exp-work app__flex"
+                    data-tip
+                    data-for={work.name}
+                    key={work.name}
+                  >
+                    <h4 className="bold-text">{work.name}</h4>
+                    <p className="p-text">{work.company}</p>
+                    <ReactTolltip
+                      id={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </ReactTolltip>
+                  </motion.div>
+                ))}
+              </motion.div>
             </motion.div>
           ))}
         </motion.div>
